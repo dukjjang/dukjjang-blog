2472895006d7e69872bb2acf1457608d4af00d43
dukjjang <jhd7292@naver.com>
2022-11-03 09:12:52 +0900

    feat: tailwind css 적용

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I love coding 😍</p>
+        <p className="text-3xl font-bold underline">꿈꾸는 신입 개발자</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
